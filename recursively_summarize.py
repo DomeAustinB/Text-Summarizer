6018821670cc2156cbf2056b8cd3cda6cba98525
DomeAustinB <100167612+DomeAustinB@users.noreply.github.com>
2022-09-02 10:53:31 -0400

    updating recursively_summarize

--- recursively_summarize.py
+++ recursively_summarize.py
@@ -1,25 +1,28 @@
 import openai
-import re
 import os
 from time import time,sleep
 import textwrap
+import re
+
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
+
 openai.api_key = open_file('openaiapikey.txt')
+
 
 def save_file(content, filepath):
     with open(filepath, 'w', encoding='utf-8') as outfile:
         outfile.write(content)
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0,
-stop=['<<END>>']):
+
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.68, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
     max_retry = 5
     retry = 0
     while True:
-        try: 
+        try:
             response = openai.Completion.create(
                 engine=engine,
                 prompt=prompt,
@@ -32,24 +35,27 @@ stop=['<<END>>']):
             text = response['choices'][0]['text'].strip()
             text = re.sub('\s+', ' ', text)
             filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)      
+            with open('Text-Summarizer/gpt3_logs/%s' % filename, 'w') as outfile:
+                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
             if retry >= max_retry:
-                return "GPT3 error: %s" & oops
-            print('Error communicating with pocketAI:', oops)
+                return "GPT3 error: %s" % oops
+            print('Error communicating with OpenAI:', oops)
             sleep(1)
 
 
 if __name__ == '__main__':
-    alltext = open_file('input.txt')
-    chunks = textwrap.wrap(alltext, 5000)
+    alltext = open_file('Text-Summarizer/input.txt')
+    chunks = textwrap.wrap(alltext, 2000)
     result = list()
+    count = 0
     for chunk in chunks:
-        prompt = open_file('prompt.txt').replace('<<SUMMARY>>', chunk)
+        count = count + 1
+        prompt = open_file('Text-Summarizer/prompt.txt').replace('<<SUMMARY>>', chunk)
+        prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
         summary = gpt3_completion(prompt)
-        print(summary)
+        print('\n\n\n', count, 'of', len(chunks), ' - ', summary)
         result.append(summary)
-    save_file('\n\n'.join(result), 'output.txt')
+    save_file('\n\n'.join(result), 'Text-Summarizer/output_%s.txt' % time())
